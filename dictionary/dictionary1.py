f25a3ac2eddcf7aa9ec16f26205a895ceb48f70f
aleeizhere <alistpaulscr7@gmail.com>
2023-08-31 20:32:03 +0500

    added dictionary question

---
+++ dictionary/dictionary1.py
@@ -0,0 +1,4 @@
+# Problem: Creating a Frequency Counter
+
+# Write a Python function called calculate_frequency that takes a list of words as input and returns a dictionary where the keys are the unique words from the list, and the values are the frequencies of those words in the list.
+
